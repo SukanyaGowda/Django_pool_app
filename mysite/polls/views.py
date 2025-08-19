d508b6bf72ec5ebf3f8cc925f2f20bb72b2926cd
SukanyaGowda <sukanyagowda02@gmail.com>
2025-08-19 20:40:20 +0530

    added requirements.txt

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+def details(request,question_id):
+    return HttpResponse("Your are looking at Question %s. " %question_id)
+
+
+def results(request,question_id):
+    response="your are looking at the result of Question %s. "
+    return HttpResponse(response % question_id)
+
+
+def vote(request,question_id)
+    return HttpResponse("you are voting on question %s. " question_id)
 # Create your views here.
