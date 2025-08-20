d5443940b0d4fef5029c2b38c92a12fe5d317769
SukanyaGowda <sukanyagowda02@gmail.com>
2025-08-20 10:26:29 +0530

    fix views page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question
 
-def details(request,question_id):
-    return HttpResponse("Your are looking at Question %s. " %question_id)
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
-def results(request,question_id):
-    response="your are looking at the result of Question %s. "
+def results(request, question_id):
+    response = "You are looking at the results of Question %s."
     return HttpResponse(response % question_id)
 
-
-def vote(request,question_id)
-    return HttpResponse("you are voting on question %s. " question_id)
-# Create your views here.
+def vote(request, question_id):
+    return HttpResponse("You are voting on Question %s." % question_id)
